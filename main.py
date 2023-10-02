87d45693f6b01962a1b69d4c93c79aaa41dd2e40
Nvsmp <oldblack26@gmail.com>
2023-10-02 08:14:32 -0300

    nsei

--- main.py
+++ main.py
@@ -1,52 +1,56 @@
-import flet as ft 
+import flet as ft
 
 def main(pagina):
-	texto_inicial = ft.Text("Raul's Zap")
-	nome_usuario = ft.TextField()
-	chat = ft.Column()
+  texto_inicial = ft.Text("Raul's Zap")
+  nome_usuario = ft.TextField()
+  chat = ft.Column()
 
-	def enviar_mensagem_tunel(mensagem):
-		if not mensagem == "":
-			#adicionar mensagem no chat
-			chat.controls.append(ft.Text(f"Sr.{mensagem['usuario']}: {mensagem['texto']}"))
-			#limpar o campo mensagem
-			campo_mensagem.value = ""
-			pagina.update()
+  def enviar_mensagem_tunel(mensagem):
+    if mensagem['texto'] != "":
+      #adicionar mensagem no chat
+      chat.controls.append(
+          ft.Text(f"Sr.{mensagem['usuario']}: {mensagem['texto']}"))
+      #limpar o campo mensagem
+      campo_mensagem.value = ""
+      pagina.update()
 
-	pagina.pubsub.subscribe(enviar_mensagem_tunel)
+  pagina.pubsub.subscribe(enviar_mensagem_tunel)
 
-	def enviar_mensagem(evento):
-		pagina.pubsub.send_all({"texto":campo_mensagem.value, "usuario":nome_usuario.value})
-		campo_mensagem.value = ""
-		pagina.update()
+  def enviar_mensagem(evento):
+    pagina.pubsub.send_all({
+        "texto": campo_mensagem.value,
+        "usuario": nome_usuario.value
+    })
+    campo_mensagem.value = ""
+    pagina.update()
 
-	campo_mensagem = ft.TextField(label="Digite uma mensagem")
-	botao_enviar_mensagem = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
+  campo_mensagem = ft.TextField(label="Digite uma mensagem")
+  botao_enviar_mensagem = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
 
-	def entrar_popup(evento):
-		pagina.add(chat)
-		popup.open = False
-		pagina.remove(botao_iniciar)
-		pagina.remove(texto_inicial)
-		pagina.add(ft.Row(
-			[campo_mensagem,botao_enviar_mensagem]
-			))
-		pagina.update()
+  def entrar_popup(evento):
+    pagina.add(chat)
+    popup.open = False
+    pagina.remove(botao_iniciar)
+    pagina.remove(texto_inicial)
+    pagina.add(ft.Row([campo_mensagem, botao_enviar_mensagem]))
+    pagina.update()
 
-	popup = ft.AlertDialog(
-		open=False,
-		modal=True,
-		title=ft.Text("Bem vindo Sr..."),
-		content=nome_usuario,
-		actions=[ft.ElevatedButton("ENTRAR", on_click=entrar_popup)],
-		)
+  popup = ft.AlertDialog(
+      open=False,
+      modal=True,
+      title=ft.Text("Bem vindo Sr..."),
+      content=nome_usuario,
+      actions=[ft.ElevatedButton("ENTRAR", on_click=entrar_popup)],
+  )
 
-	def entrar_chat(evento):
-		pagina.dialog = popup
-		popup.open = True
-		pagina.update()
-		
-	botao_iniciar = ft.ElevatedButton("Iniciar chat", on_click=entrar_chat)
-	pagina.add(texto_inicial)
-	pagina.add(botao_iniciar)
-ft.app(target=main, view=ft.WEB_BROWSER)	#
+  def entrar_chat(evento):
+    pagina.dialog = popup
+    popup.open = True
+    pagina.update()
+
+  botao_iniciar = ft.ElevatedButton("Iniciar chat", on_click=entrar_chat)
+  pagina.add(texto_inicial)
+  pagina.add(botao_iniciar)
+
+
+ft.app(target=main, view=ft.WEB_BROWSER)  #
